a390c508d81d0f7a99130df557baaaf6ee07550d
Juan Camacho <68569950+Juan321654@users.noreply.github.com>
2025-07-03 22:35:19 -0400

    simple game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int triesLeft = 10;
+string wordToGuess = "hello";
+
+while (triesLeft > 0) {
+    Console.WriteLine($"You have {triesLeft} tries left.");
+    Console.Write("Guess the word: ");
+    string userGuess = Console.ReadLine();
+
+    if (userGuess.Equals(wordToGuess, StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine("Congratulations! You've guessed the word!");
+        break;
+    } else {
+        triesLeft--;
+        Console.WriteLine("Wrong guess. Try again.");
+    }
+}
